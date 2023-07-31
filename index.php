0f842417ead5a8abc2227e1d4eca7aa0b0d6c075
Boudjadjasamira <boudjadjasamira@gmail.com>
2023-07-31 16:25:14 +0200

    Update project

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php include './view/common/header.php'; ?>
+
+  <!-- Header avec image à gauche et à droite -->
+  <div class="container mt-4">
+            <div class="row">
+
+                <div class="col-6">
+                    <img src="./public/img/header_img.png" alt="Image gauche" class="img-fluid">
+                </div>
+
+                <div class="col-6">
+                    <img src="./public/img/Bannheader.png" alt="Image droite" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="row mt-4">
+                <div class="col-12 text-center">
+                    <button class="btn btn-primary">Découvrir</button>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Section presentation -->
+    <div class="container mt-5">
+      <div class="row">
+
+          <div class="col-12 col-md-6">
+              <img src="./public/img/AuthenticTaste.png" alt="Image" class="img-fluid">
+          </div>
+
+          <div class="col-12 col-md-6 text-white">
+              <h2>Des produits frais, de qualité.</h2>
+              <p>
+                Nos mets sont préparés et cuisinés à la commande sur
+                place, pour vous assurer une expérience gustative unique.
+              </p>
+
+              <h2 class="fw-bold">Entre tradition et innovation</h2>
+              <p class="">
+                Notre chef s’inspire de vos envies pour confectionner une
+                carte qui répond à vos attentes mais peut aussi vous
+                surprendre. Notre équipe a développé un véritable
+                savoir-faire dans l’art des sushi pour vous proposer une
+                cuisine simple et savoureuse.
+              </p>
+          </div>
+      </div>
+  </div>
+
+  <?php include('./view/common/footer.php'); ?>
