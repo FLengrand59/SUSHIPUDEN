82131091fd2f715514a08e03c20cb9535a75d7b6
FLengrand59 <f.lengrand@hotmail.com>
2023-08-28 13:57:01 +0200

    forEach de la lecture de la fonction Read
    J'ai forEach (je compte re checker ça, car je pense que tous n'est pas bon) ma fonction forEach de mon read de crud pour les reservations (j'ai fait ma fonction, puis appeler dans le controller pour l'à mettre dans une variable + require once, puis j'ai include ça dans la view reservation

--- view/backoffice/resa.php
+++ view/backoffice/resa.php
@@ -1,73 +1,74 @@
-<?php 
+<?php
 
-include './common/menu.php'; 
+include './common/menu.php';
 include './../../controller/readResaBackController.php';
+include "../../controller/readController.php";
 
 ?>
-    <!-- main content -->
-    <main class="p-4 mh-100">
-        <div class="row">
-            <div class="col-lg-12">    
-                <div class="card shadow mb-4  bg">
-                    <a href="#" class="d-block card-header py-3" role="button" aria-expanded="true"  >
-                        <h6 class="m-0 ic">Récapitulatif des réservations</h6>
-                    </a>
+<!-- main content -->
+<main class="p-4 mh-100">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card shadow mb-4  bg">
+                <a href="#" class="d-block card-header py-3" role="button" aria-expanded="true">
+                    <h6 class="m-0 ic">Récapitulatif des réservations</h6>
+                </a>
 
-                            
-                                <div class="row">
-                                    <div class="col-12">      
 
-                                        <div class="table-responsive table-striped"> 
-                                        <table class="table table-hover">
-                                        <thead class="thead-light">
-                                            <tr">
-                                            <th scope="col">Nom</th>
-                                            <th scope="col">Prénom</th>
-                                            <th scope="col">N° Tel</th>
-                                            <th scope="col">Email</th>
-                                            <th scope="col">Date</th>
-                                            <th scope="col">Couverts</th>
-                                            <th scope="col">Service</th>
-                                            <th scope="col">État</th>
-                                            <th scope="col">Options</th>
-                                            </tr>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php foreach ($recupResa as $row) { ?>
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="table-responsive table-striped">
+                            <table class="table table-hover">
+                                <thead class="thead-light">
+                                    <tr">
+                                        <th scope="col">Nom</th>
+                                        <th scope="col">Prénom</th>
+                                        <th scope="col">N° Tel</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Couverts</th>
+                                        <th scope="col">Service</th>
+                                        <th scope="col">État</th>
+                                        <th scope="col">Options</th>
+                                        </tr>
+                                        </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($recupReservation as $readReservation) { ?>
                                         <tr>
-                                            <td><?php echo $row['nom']; ?></td>
-                                            <td><?php echo $row['prenom']; ?></td>
-                                            <td><?php echo $row['telephone']; ?></td>
-                                            <td><?php echo $row['email']; ?></td>
+                                            <td><?php echo $row['Uzumaki']; ?></td>
+                                            <td><?php echo $row['Naruto']; ?></td>
+                                            <td><?php echo $row['0701020304']; ?></td>
+                                            <td><?php echo $row['konoha@gmail.com']; ?></td>
                                             <!-- Formatage de la date -->
-                                            <td><?php echo date("d/m/Y", strtotime($row['date'])); ?></td>
-                                            <td><?php echo $row['couverts']; ?></td>
+                                            <td><?php echo date("20/08/2023", strtotime($row['date'])); ?></td>
+                                            <td><?php echo $row['2']; ?></td>
                                             <td>
-                                            <?php if ($row['service'] === "0") { ?>
+                                                <?php if ($row['midi'] === "0") { ?>
                                                     <?php echo "Midi"; ?>
-                                            <?php } else { ?>
-                                                <?php echo "Soir"; ?>
-                                            <?php } ?>
+                                                <?php } else { ?>
+                                                    <?php echo "Soir"; ?>
+                                                <?php } ?>
                                             </td>
                                             <td>
-                                        <!-- Selon l'etat on affiche un badge différent -->
-                                            <?php if ($row['etat'] === "0") { ?>
-                                                <span class="badge badge-pill badge-warning">
-                                                    <?php echo "En attente"; ?>
-                                                </span>
-                                            <?php } elseif ($row['etat'] === "1") { ?>
-                                                <span class="badge badge-pill badge-success">
-                                                    <?php echo "Confirmé"; ?>
-                                                </span>
+                                                <!-- Selon l'etat on affiche un badge différent -->
+                                                <?php if ($row['etat'] === "0") { ?>
+                                                    <span class="badge badge-pill badge-warning">
+                                                        <?php echo "En attente"; ?>
+                                                    </span>
+                                                <?php } elseif ($row['etat'] === "1") { ?>
+                                                    <span class="badge badge-pill badge-success">
+                                                        <?php echo "Confirmé"; ?>
+                                                    </span>
                                                 <?php } elseif ($row['etat'] === "2") { ?>
-                                                <span class="badge badge-pill badge-danger">
-                                                    <?php echo "Annulé"; ?>
-                                                </span>
-                                            <?php } ?>
+                                                    <span class="badge badge-pill badge-danger">
+                                                        <?php echo "Annulé"; ?>
+                                                    </span>
+                                                <?php } ?>
                                             </td>
                                             <td>
-                                            <div class="dropdown ml-auto">
+                                                <div class="dropdown ml-auto">
                                                     <button class="btn d-flex align-items-center" id="logout-dropdown" data-toggle="dropdown" aria-expanded="false">
                                                         <i class="iconoir-more-vert icme pb-1"></i>
                                                     </button>
@@ -79,23 +80,23 @@ include './../../controller/readResaBackController.php';
                                                     </div>
                                                 </div>
                                             </td>
-                                        </tr> 
-                                        <?php } ?>
+                                        </tr>
+                                    <?php } ?>
 
 
 
-                                            
-                                    
-                                        </tbody>
-                                        </table>
-                                        </div>
-                                    </div>
-                                </div>
 
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
-    </main>
+    </div>
+</main>
 
 
 <?php include './common/footer.php'; ?>
