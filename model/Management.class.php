8b0b0e08e645adfffe63e2ae2c700ef7099ccfcc
Boudjadjasamira <boudjadjasamira@gmail.com>
2023-08-31 15:39:42 +0200

    fix getName

--- model/Management.class.php
+++ model/Management.class.php
@@ -157,9 +157,9 @@ class Management
                 'nom' => $reservation->getNom(),
                 'prenom' => $reservation->getPrenom(),
                 'email' => $reservation->getEmail(),
-                'telephone' => $reservation->getTel(),
+                'telephone' => $reservation->getTelephone(),
                 'date' => $reservation->getDate(),
-                'couverts' => $reservation->getNbre_Couverts(),
+                'couverts' => $reservation->getCouverts(),
                 'service' => $reservation->getService(),
                 'etat' => $reservation->getEtat(),
             ];
