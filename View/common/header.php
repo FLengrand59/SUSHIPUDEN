9ec5409e1fc960f29afe736a366f1edfa442935f
FLengrand59 <f.lengrand@hotmail.com>
2023-10-31 16:15:48 +0100

    Update header.php

--- View/common/header.php
+++ View/common/header.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Sushpuden, restaurant japonais à Cambrai, propose une délicieuse sélection de plats japonais, préparés avec des ingrédients frais. Venez déguster nos spécialités japonaises dans une ambiance authentique à Cambrai.">
-    <meta name="keywords" content="Sushpuden, restaurant japonais Cambrai, gyoza, crevette, tempura, maki, sushi, moshi, doryiaki, produits frais, spécialités japonaises, cuisine japonaise, Cambrai, restaurant authentique">
     <title>Sushipuden</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
